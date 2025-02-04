3d4d988883473c7a6b3c910b5bf9d56fc93d7d95
Ashok Singh <singh.ashok1@indiamart.com>
2025-02-04 21:42:18 +0530

    custom react code

--- customReact/CustomReact.js
+++ customReact/CustomReact.js
@@ -19,10 +19,21 @@ if (mainContainer) {
 }
 
 function customRender(reactElement, mainContainer) {
-   const domElement = document.createElement(reactElement.type);
-   domElement.setAttribute('href', reactElement.props.href);
-   domElement.setAttribute('target', reactElement.props.target);
-   domElement.textContent = reactElement.children; // Safer than innerHTML
+//    const domElement = document.createElement(reactElement.type);
+//    domElement.setAttribute('href', reactElement.props.href);
+//    domElement.setAttribute('target', reactElement.props.target);
+//    domElement.textContent = reactElement.children; // Safer than innerHTML
 
-   mainContainer.appendChild(domElement);
+//    mainContainer.appendChild(domElement);
+
+
+   const domElement= document.createElement(reactElement.type);
+   domElement.innerHTML=reactElement.children;
+   for (const key in reactElement.props) {
+    if (Object.prototype.hasOwnProperty.call(reactElement.props, key)) {
+        const element = reactElement.props[key];
+        domElement.setAttribute(key,element);
+        
+    }
+   }
 }
