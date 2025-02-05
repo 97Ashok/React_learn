69881bade293617e957aed66f5ce40dc7750f192
Ashok Singh <singh.ashok1@indiamart.com>
2025-02-05 14:21:27 +0530

    custom react code

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+import React ,{ StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+
+const reactElement= React.createElement(
+  'a', {href:'http://www.google.com', target:'_blank'}, 'click me'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
+  // <StrictMode>
+  //  {/* <App /> */}
+   
+  // </StrictMode>,
 )
